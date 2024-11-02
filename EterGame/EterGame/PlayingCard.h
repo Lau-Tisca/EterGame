f74a2263873cb6481c1ecf63f19da1773ae1d644
IacobGiulia <giuliaiacob004@gmail.com>
2024-11-02 21:53:46 +0200

    Completare clasa PlayingCard

--- EterGame/EterGame/PlayingCard.h
+++ EterGame/EterGame/PlayingCard.h
@@ -8,5 +8,13 @@ public:
 	PlayingCard(const PlayingCard& other);     //constructor de copiere	
 	uint8_t getValue();
 	CardType getType();
+	int getX();
+	int getY();
+	void setCardPosition(int x, int y);
+	bool canBePlacedOver(const PlayingCard& other) const;
+private:
+	uint8_t m_value;
+	CardType m_type;
+	int m_x, m_y; //coordonatele cartii de joc in matrice
 };
 
