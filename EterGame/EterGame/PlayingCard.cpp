d1614be7985376dfe3d1ba96bb1b8a3aeb595ed2
IacobGiulia <giuliaiacob004@gmail.com>
2024-11-02 22:15:08 +0200

    Implementare functii in PlayingCard.cpp

--- EterGame/EterGame/PlayingCard.cpp
+++ EterGame/EterGame/PlayingCard.cpp
@@ -1,1 +1,33 @@
 #include "PlayingCard.h"
+
+PlayingCard::PlayingCard(uint8_t Value, CardType Type, uint8_t x, uint8_t y) : 
+	m_value(Value), m_type(Type), m_x(x), m_y(y){}
+
+PlayingCard::PlayingCard(const PlayingCard& other)
+	: m_value(other.m_value), m_type(other.m_type), m_x(other.m_x), m_y(other.m_y) {}
+
+uint8_t PlayingCard::getValue () const
+{
+	return m_value;
+}
+PlayingCard::CardType PlayingCard::getType() const
+{
+	return m_type;
+}
+uint8_t PlayingCard::getX() const
+{
+	return m_x;
+}
+uint8_t PlayingCard::getY() const
+{
+	return m_y;
+}
+void PlayingCard::setCardPosition(uint8_t x, uint8_t y)
+{
+	m_x = x;
+	m_y = y;
+}
+bool PlayingCard::canBePlacedOver(const PlayingCard& other) const
+{
+	return m_value > other.m_value && other.getType() != CardType::eter;
+}
--- EterGame/EterGame/PlayingCard.h
+++ EterGame/EterGame/PlayingCard.h
@@ -4,17 +4,18 @@ class PlayingCard
 {
 public:
 	enum class CardType { numbered, eter };
-	PlayingCard(uint8_t Value, CardType Type); //constructor
+	PlayingCard(uint8_t Value, CardType Type, uint8_t x=-1, uint8_t y=-1); //constructor
 	PlayingCard(const PlayingCard& other);     //constructor de copiere	
-	uint8_t getValue();
-	CardType getType();
-	int getX();
-	int getY();
-	void setCardPosition(int x, int y);
+	uint8_t getValue() const;
+	CardType getType() const;
+	uint8_t getX() const;
+	uint8_t getY() const;
+	void setCardPosition(uint8_t x, uint8_t y);
 	bool canBePlacedOver(const PlayingCard& other) const;
 private:
 	uint8_t m_value;
 	CardType m_type;
-	int m_x, m_y; //coordonatele cartii de joc in matrice
+	uint8_t m_x, m_y;
+	//coordonatele cartii de joc in matrice
 };
 
