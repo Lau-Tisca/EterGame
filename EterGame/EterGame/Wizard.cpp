1ae42eb44b02713a54b979a20bbacd63a0f26733
Antonio Samir Vicas <antonio.vicas@student.unitbv.ro>
2024-11-15 12:41:11 +0200

    incepere functie de utilizat puteri

--- EterGame/EterGame/Wizard.cpp
+++ EterGame/EterGame/Wizard.cpp
@@ -1,4 +1,4 @@
-#include "Wizard.h"
+﻿#include "Wizard.h"
 #include <iostream>
 
 Wizard::Wizard(const std::u8string& name, const std::u8string& power)
@@ -11,4 +11,28 @@ std::u8string Wizard::getName() const {
 
 std::u8string Wizard::getPowerName() const {
     return m_power;
+}
+
+// puterile
+bool Wizard::usePower(Board& board) {
+    if (m_powerUsed) {
+        std::cout << "Puterea vrăjitorului " << std::string(m_name.begin(), m_name.end()) << " a fost deja utilizată!" << std::endl;
+        return false;
+    }
+
+    if (m_power == "Explosion") {
+        // Exemplu: Șterge toate cărțile dintr-un rând
+        int row = 0;  // Aici poți primi input pentru a alege rândul
+        for (auto& cell : board.getLine()) {
+            cell = nullptr;
+        }
+        std::cout << "Explozie activată de " << std::string(m_name.begin(), m_name.end()) << "!" << std::endl;
+    }
+    else if (m_power == "Illusion") {
+        // Exemplu: Plasează o carte falsă
+        std::cout << std::string(m_name.begin(), m_name.end()) << " a plasat o carte iluzie!" << std::endl;
+    }
+
+    m_powerUsed = true;  // Marchez că puterea a fost utilizată
+    return true;
 }
