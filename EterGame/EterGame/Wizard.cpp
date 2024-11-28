c4612f0b0cb016a5750db0f3fe815e30afda1fcd
Tișcă Laurențiu-Ștefan <laurentiu.tisca@student.unitbv.ro>
2024-11-28 16:50:26 +0200

    Wizard 1

--- EterGame/EterGame/Wizard.cpp
+++ EterGame/EterGame/Wizard.cpp
@@ -14,6 +14,54 @@ std::u8string Wizard::getPowerName() const {
 }
 
 // puterile
+void Wizard::useAbility(GameBoard& board, int row, int col, Player& currentPlayer, int destRow, int destCol) {
+    if (usedThisMatch) {
+        throw std::runtime_error("The magical power has already been used this match!");
+    }
+    if (usedThisGame) {
+        throw std::runtime_error("The magical power has already been used this game!");
+    }
+
+    usedThisGame = true;
+    usedThisMatch = true;
+
+    switch (power) {
+    case MagicPower::REMOVE_OPPONENT_CARD_OVER_OWN:
+        board.removeOpponentCardOverOwn(row, col, currentPlayer);
+        break;
+    case MagicPower::REMOVE_ROW_WITH_OWN_CARD:
+        board.removeRowWithOwnCard(row, currentPlayer);
+    case MagicPower::COVER_OPPONENT_CARD:
+        board.coverOpponentCard(row, col, currentPlayer);
+    case MagicPower::CREATE_HOLE:
+        board.createHole(row, col);
+    case MagicPower::MOVE_STACK_WITH_OWN_CARD:
+        if (destRow == -1 || destCol == -1) {
+            throw std::runtime_error("Destination row and column must be specified for this ability!");
+        }
+        board.moveStackWithOwnCard(row, col, destRow, destCol, currentPlayer);
+    case MagicPower::GAIN_EXTRA_CARD: {
+        board.placeEtherCard(row, col, currentPlayer);
+    case MagicPower::MOVE_STACK_WITH_OPPONENT_CARD:
+        if (destRow == -1 || destCol == -1) {
+            throw std::runtime_error("Destination row and column must be specified for this ability!");
+        }
+        board.moveStackWithOpponentCard(row, col, destRow, destCol, currentPlayer);
+    case MagicPower::MOVE_ROW_TO_EDGE:
+        if (destRow == -1 || destCol == -1) {
+            throw std::runtime_error("Destination edge must be specified for this ability!");
+        }
+        board.moveRowToEdge(row, col, destRow, destCol);
+        break;
+    }
+                                    break;
+    default:
+        throw std::runtime_error("Invalid magical power!");
+    }
+
+    std::cout << name << " used their magical power!\n";
+}
+
 bool Wizard::usePower(Board& board, Player& currentPlayer, Player& opponent) {
     if (m_powerUsed) {
         std::cout << "Puterea vrăjitorului " << std::string(m_name.begin(), m_name.end()) << " a fost deja utilizată!" << std::endl;
