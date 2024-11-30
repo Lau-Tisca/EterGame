71b80b9d4bed72c7414d2df5637e9c786081c047
antoniov15 <antonio.vicas@student.unitbv.ro>
2024-12-01 00:59:59 +0200

    wizard.cpp

--- EterGame/EterGame/Wizard.cpp
+++ EterGame/EterGame/Wizard.cpp
@@ -7,6 +7,11 @@
 #include <iostream>
 #include <string>
 
+/*
+Wizard::Wizard(const std::string& name, MagicPower power)
+    : name(name), power(power), usedThisGame(false), usedThisMatch(false) {
+}*/
+
 Wizard::Wizard(const std::u8string& name, MagicPower power)
     : name(name), power(power) {
 }
@@ -34,22 +39,28 @@ void Wizard::useAbility(GameBoard& board, int row, int col, Player& currentPlaye
         break;
     case MagicPower::REMOVE_ROW_WITH_OWN_CARD:
         board.removeRowWithOwnCard(row, currentPlayer);
+        break;
     case MagicPower::COVER_OPPONENT_CARD:
         board.coverOpponentCard(row, col, currentPlayer);
+        break;
     case MagicPower::CREATE_HOLE:
         board.createHole(row, col);
+        break;
     case MagicPower::MOVE_STACK_WITH_OWN_CARD:
         if (destRow == -1 || destCol == -1) {
             throw std::runtime_error("Destination row and column must be specified for this ability!");
         }
         board.moveStackWithOwnCard(row, col, destRow, destCol, currentPlayer);
+        break;
     case MagicPower::GAIN_EXTRA_CARD: {
         board.placeEtherCard(row, col, currentPlayer);
+        break;
     case MagicPower::MOVE_STACK_WITH_OPPONENT_CARD:
         if (destRow == -1 || destCol == -1) {
             throw std::runtime_error("Destination row and column must be specified for this ability!");
         }
         board.moveStackWithOpponentCard(row, col, destRow, destCol, currentPlayer);
+        break;
     case MagicPower::MOVE_ROW_TO_EDGE:
         if (destRow == -1 || destCol == -1) {
             throw std::runtime_error("Destination edge must be specified for this ability!");
@@ -57,7 +68,6 @@ void Wizard::useAbility(GameBoard& board, int row, int col, Player& currentPlaye
         board.moveRowToEdge(row, col, destRow, destCol);
         break;
     }
-                                    break;
     default:
         throw std::runtime_error("Invalid magical power!");
     }
