1e52046a3d711ebbc9df6b63cabdacbce10933b5
antoniov15 <antonio.vicas@student.unitbv.ro>
2024-12-01 00:50:37 +0200

    main

--- EterGame/EterGame/Source.cpp
+++ EterGame/EterGame/Source.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "Game.h"
 
 int main() {
-	std::cout << "kasjkjashkj\n";
-	std::cout << "dada ";
-	std::cout << "\ntEST din partea mea";
+    srand(static_cast<unsigned>(time(0)));
+
+    Wizard wizard1("Fire Mage", MagicPower::REMOVE_ROW_WITH_OWN_CARD);
+    Wizard wizard2("Ice Mage", MagicPower::REMOVE_OPPONENT_CARD_OVER_OWN);
+
+    Game game(3, "Player1", "Player2", wizard1, wizard2);
+    game.start();
 
 
 	return 0;
